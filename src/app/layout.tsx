03e5b168f0eacb56097b4bb1ceed019d04c8ec94
Sunkanmi <oguntimehins@gmail.com>
2025-11-18 00:54:06 -0800

    commit changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Image from "next/image";
+import Script from "next/script";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,6 +27,37 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+        <head>
+
+
+           <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+    !function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1397940205049713');
+fbq('track', 'PageView');
+  `}
+        </Script> 
+
+
+ <noscript id="facebook-pixel-script">
+          <Image
+            height={1}
+            width={1}
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1397940205049713&ev=PageView&noscript=1"
+            alt="Facebook Pixel"
+            unoptimized
+          />
+        </noscript> 
+
+        </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
@@ -32,3 +66,9 @@ export default function RootLayout({
     </html>
   );
 }
+
+
+
+
+
+ 
